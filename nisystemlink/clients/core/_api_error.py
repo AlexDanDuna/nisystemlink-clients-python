01d1bb3756707fd7c260eeff8c3943827db0f438
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2026-07-13 18:45:30 +0300

    Clarify ApiError docstring for error object structure
    Updated the docstring to clarify the structure of the error object.

--- nisystemlink/clients/core/_api_error.py
+++ nisystemlink/clients/core/_api_error.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """Implementation of ApiError."""
-# WHAAT
 from typing import List
 
 from ._uplink._json_model import JsonModel
 
 
 class ApiError(JsonModel):
-    """Represents the standard error structure for SystemLink API responses."""
+    """Represents the standard error object structure for SystemLink API responses."""
 
     name: str | None = None
     """String error code."""
