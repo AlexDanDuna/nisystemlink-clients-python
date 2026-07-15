2629e153bef579329d561b0e38e9d247080dd204
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2026-07-15 10:49:17 +0300

    fix: try fix commit

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from pydantic import Extra
 
 
 def pytest_addoption(parser):
-    """Register command line flags that tell us how to connect to the web server.
+    """Register command line flags that tell us how to connect to the web servers.
 
     Note that command line flags can also be added to the ``[pytest]`` section of
     ``tox.ini``, in an ``addopts`` setting.
